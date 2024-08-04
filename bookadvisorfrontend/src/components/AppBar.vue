7dc8255df12f1240722a2cbe2fbe0f84aef37c7c
sitMCella <phcella@libero.it>
2024-08-04 11:33:33 +0200

    Get Projects feature

--- bookadvisorfrontend/src/components/AppBar.vue
+++ bookadvisorfrontend/src/components/AppBar.vue
@@ -4,10 +4,10 @@ import axios from 'axios'
 </script>
 
 <template>
-    <v-app-bar :elevation="2">
-        <template v-slot:prepend>
-            <v-app-bar-nav-icon></v-app-bar-nav-icon>
-        </template>
-        <v-app-bar-title>Book Advisor</v-app-bar-title>
-    </v-app-bar>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+    <v-app-bar-title>Book Advisor</v-app-bar-title>
+  </v-app-bar>
 </template>
--- bookadvisorfrontend/src/components/Navigation.vue
+++ bookadvisorfrontend/src/components/Navigation.vue
@@ -4,23 +4,28 @@ import axios from 'axios'
 </script>
 
 <template>
-    <v-navigation-drawer rail>
-        <v-list density="compact" nav>
-            <v-list-item prepend-icon="mdi-home" title="Projects" value="projects" :to="{path: '/'}"></v-list-item>
-            <v-divider></v-divider>
-            <v-list-item
-            prepend-icon="mdi-view-grid-outline"
-            title="Project Map"
-            value="projectMap"
-            :to="{path: '/projectmap'}"
-            ></v-list-item>
-            <v-divider></v-divider>
-            <v-list-item
-            prepend-icon="mdi-book-open-blank-variant-outline"
-            title="Project Book"
-            value="projectBook"
-            ></v-list-item>
-            <v-divider></v-divider>
-        </v-list>
-    </v-navigation-drawer>
+  <v-navigation-drawer rail>
+    <v-list density="compact" nav>
+      <v-list-item
+        prepend-icon="mdi-home"
+        title="Projects"
+        value="projects"
+        :to="{ path: '/' }"
+      ></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item
+        prepend-icon="mdi-view-grid-outline"
+        title="Project Map"
+        value="projectMap"
+        :to="{ path: '/projectmap' }"
+      ></v-list-item>
+      <v-divider></v-divider>
+      <v-list-item
+        prepend-icon="mdi-book-open-blank-variant-outline"
+        title="Project Book"
+        value="projectBook"
+      ></v-list-item>
+      <v-divider></v-divider>
+    </v-list>
+  </v-navigation-drawer>
 </template>
